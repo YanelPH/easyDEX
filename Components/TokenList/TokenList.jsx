525018aefb8f9aec1c94c69534549dda87656b65
Yanel PHINERA-HORTH <yphinerahorth@gmail.com>
2024-01-05 12:12:32 +0100

    Token List Model done

--- Components/TokenList/TokenList.jsx
+++ Components/TokenList/TokenList.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import Image from "next/image";
 
-const TokenList = () => {
-  return <div>TokenList</div>;
+//Internal Import
+import Style from "./TokenList.module.css";
+import images from "../../assets";
+
+const TokenList = ({ tokenDate, setOpenBox }) => {
+  const data = [1, 2, 3, 4, 5, 6, 7];
+
+  return (
+    <div className={Style.TokenList}>
+      <p
+        className={Style.TokenList_close}
+        onClick={() => setOpenTokenBox(false)}
+      >
+        <Image src={images.close} alt="close" width={50} height={50} />
+      </p>
+      <div className={Style.TokenList_title}>
+        <h2>Your Token List</h2>
+      </div>
+      {data.map((element, index) => (
+        <div className={Style.TokenList_box}>
+          <div className={Style.TokenList_box_info}>
+            <p className={Style.TokenList_box_info_symbol}>Hey</p>
+            <p>
+              <span>35</span> GOLD COIN
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default TokenList;
