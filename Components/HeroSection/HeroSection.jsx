1c44704bcdaa2f80ca0237691f7b1e3d99f0f94c
Yanel PHINERA-HORTH <yphinerahorth@gmail.com>
2024-01-06 14:27:56 +0100

    Swap Token Model done

--- Components/HeroSection/HeroSection.jsx
+++ Components/HeroSection/HeroSection.jsx
@@ -1,7 +1,96 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import Image from "next/image";
 
-const HeroSection = () => {
-  return <div>HeroSection</div>;
+//Internal Import
+import Style from "./HeroSection.module.css";
+import images from "../../assets";
+import { Token, SearchToken } from "../index";
+
+const HeroSection = ({ accounts, tokenData }) => {
+  //USESTATE
+  const [openSetting, setOpenSetting] = useState(false);
+  const [openToken, setOpenToken] = useState(false);
+  const [openTokensTwo, setOpenTokensTwo] = useState(false);
+
+  //Token 1
+  const [tokenOne, setTokenOne] = useState({
+    name: "",
+    images: "",
+  });
+  //Token2
+  const [tokenTwo, setTokenTwo] = useState({
+    name: "",
+    images: "",
+  });
+  //JSX
+  return (
+    <div className={Style.HeroSection}>
+      <div className={Style.HeroSection_box}>
+        <div className={Style.HeroSection_box_heading}>
+          <p>Swap</p>
+          <div className={Style.HeroSection_box_heading_img}>
+            <Image
+              src={images.close}
+              alt="image"
+              width={50}
+              height={50}
+              onClick={() => setOpenSetting(true)}
+            />
+          </div>
+        </div>
+        <div className={Style.HeroSection_box_input}>
+          <input type="text" placeholder="0" />
+          <button onClick={() => openToken(true)}>
+            <Image
+              src={tokenOne.images || images.etherlogo}
+              width={20}
+              height={20}
+              alt="ether"
+            />
+            {tokenOne.name || "ETH"}
+            <small>9474</small>
+          </button>
+        </div>
+        <div className={Style.HeroSection_box_input}>
+          <input type="text" placeholder="0" />
+          <button onClick={() => openToken(true)}>
+            <Image
+              src={tokenTwo.images || images.etherlogo}
+              width={20}
+              height={20}
+              alt="ether"
+            />
+            {tokenTwo.name || "ETH"}
+            <small>9474</small>
+          </button>
+        </div>
+        {accounts ? (
+          <button className={Style.HeroSection_box_btn}>Connect Wallet</button>
+        ) : (
+          <button className={Style.HeroSection_box_btn} onClick={() => {}}>
+            Swap
+          </button>
+        )}
+      </div>
+
+      {openSetting && <Token openSetting={openSetting} />}
+
+      {openToken && (
+        <SearchToken
+          openToken={setOpenToken}
+          tokens={setTokenOne}
+          tokenData={tokenData}
+        />
+      )}
+      {openToken && (
+        <SearchToken
+          openToken={setOpenTokensTwo}
+          tokens={setTokenTwo}
+          tokenData={tokenData}
+        />
+      )}
+    </div>
+  );
 };
 
 export default HeroSection;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+
+//Internal Import
+import { HeroSection } from "../Components/index";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <HeroSection accounts="hey" tokenData="DATA" />
+    </div>
+  );
 };
 
 export default Home;
