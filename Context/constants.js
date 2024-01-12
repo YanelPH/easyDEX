ff682fbe9a207693367da7b8d64e97ff94db5389
Yanel PHINERA-HORTH <yphinerahorth@gmail.com>
2024-01-12 10:52:04 +0100

    Context and ABI Added

---
+++ Context/constants.js
@@ -0,0 +1,26 @@
+import booToken from "./BooToken.json";
+import lifeToken from "./LifeToken.json";
+import singleSwapToken from "./SingleSwapToken.json";
+import swapMultiHop from "./SwapMultiHop.json";
+
+//BooToken
+export const BooTokenAddress = "0xAE246E208ea35B3F23dE72b697D47044FC594D5F";
+export const BooTokenABI = booToken.abi;
+
+//BooToken
+export const LifeTokenAddress = "0x82BBAA3B0982D88741B275aE1752DB85CAfe3c65";
+export const LifeTokenABI = lifeToken.abi;
+
+//SingleSwapToken
+export const SingleSwapTokenAddress =
+  "0x084815D1330eCC3eF94193a19Ec222C0C73dFf2d";
+export const SingleSwapTokenABI = singleSwapToken.abi;
+
+//SwapMultiHop
+export const SwapMultiHopAddress = "0x76a999d5F7EFDE0a300e710e6f52Fb0A4b61aD58";
+export const SwapMultiHopABI = swapMultiHop.abi;
+
+// BOO deployed to 0xAE246E208ea35B3F23dE72b697D47044FC594D5F
+// LifeToken deployed to 0x82BBAA3B0982D88741B275aE1752DB85CAfe3c65
+// SingleSwapToken deployed to 0x084815D1330eCC3eF94193a19Ec222C0C73dFf2d
+// SwapMultiHop deployed to 0x76a999d5F7EFDE0a300e710e6f52Fb0A4b61aD58
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,12 +1,39 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy();
+  //ERC20 BOO TOKEN
+  const BooToken = await hre.ethers.getContractFactory("BooToken");
+  const booToken = await BooToken.deploy();
 
-  await lock.deployed();
+  await booToken.deployed();
 
-  console.log(` deployed to ${lock.address}`);
+  console.log(`BOO deployed to ${booToken.address}`);
+
+  //ERC20 Life TOKEN
+  const LifeToken = await hre.ethers.getContractFactory("LifeToken");
+  const lifeToken = await LifeToken.deploy();
+
+  await lifeToken.deployed();
+
+  console.log(`LifeToken deployed to ${lifeToken.address}`);
+
+  //SingleSwapToken
+  const SingleSwapToken = await hre.ethers.getContractFactory(
+    "SingleSwapToken"
+  );
+  const singleSwapToken = await SingleSwapToken.deploy();
+
+  await singleSwapToken.deployed();
+
+  console.log(`SingleSwapToken deployed to ${singleSwapToken.address}`);
+
+  //SwapMultiHop
+  const SwapMultiHop = await hre.ethers.getContractFactory("SwapMultiHop");
+  const swapMultiHop = await SwapMultiHop.deploy();
+
+  await swapMultiHop.deployed();
+
+  console.log(`SwapMultiHop deployed to ${swapMultiHop.address}`);
 }
 
 main().catch((error) => {
